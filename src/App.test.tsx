d3259606dd7085bc9b309ec23cdacceb506302c9
Rik Sportel <riksportel@hotmail.com>
2025-09-28 13:22:59 +0300

    Clean up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Rik Sportel text on the site', () => {
   render(<App />);
   const linkElement = screen.getByText(/Rik Sportel/i);
   expect(linkElement).toBeInTheDocument();
